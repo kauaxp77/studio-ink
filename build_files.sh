8641bdcdc53c3c3360ab8b27c98d29c41861e55f
kauaxp77? <Wendesonkaua11@gmail.com>
2026-08-15 12:52:35 -0300

    chore(deploy): add automate migration and admin injection script

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 python3 -m pip install -r requirements.txt --break-system-packages
 python3 manage.py collectstatic --noinput
+python3 manage.py migrate --noinput
+python3 manage.py shell -c "from django.contrib.auth.models import User; u, _ = User.objects.get_or_create(username='admin', defaults={'email': 'admin@example.com'}); u.is_superuser = True; u.is_staff = True; u.set_password('123'); u.save();"
